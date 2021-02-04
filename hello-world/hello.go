584ba200e36978ab4d58f016e361e0b5ee865eaa
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-02-04 07:42:11 -0300

    Calling the external module (greetings) in Hello-World module

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"rodrigodonizettio.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello world from Go!")
 	fmt.Println(quote.Glass())
+
+	message := greetings.Hail("donizetti")
+	fmt.Println(message)
 }

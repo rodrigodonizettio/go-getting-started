dcbaa36755185a176153439c8bacbca3bebbfa8f
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-02-08 07:21:21 -0300

    Handling error in Go and implementing multiple return function (string and error)

--- create-module/greetings.go
+++ create-module/greetings.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 )
 
-//   FuncName(pars)       returnType {
-func Hail(name string) string {
+//   FuncName(pars)       returnTypes {
+func Hail(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
+	if name == "" {
+		return "", errors.New("Empty Name")
+	}
 
 	// var message string (":=" below does the same. It declares and initialize a variable in one line. The value in right determines the "type")
 	message := fmt.Sprintf("Hail to the king, %v!", name)
 	fmt.Println(message)
-	return message
+	return message, nil
 }

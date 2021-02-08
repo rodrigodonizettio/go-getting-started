3a12116873ace3ee5d379444d0ed9bda335b2b73
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-02-08 07:53:10 -0300

    Removing duplicated log for Hail function

--- create-module/greetings.go
+++ create-module/greetings.go
@@ -15,7 +15,5 @@ func Hail(name string) (string, error) {
 	}
 
 	// var message string (":=" below does the same. It declares and initialize a variable in one line. The value in right determines the "type")
-	message := fmt.Sprintf("Hail to the king, %v!", name)
-	fmt.Println(message)
 	return message, nil
 }

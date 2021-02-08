24b104cedd56cdac1aa8ca4c1802d2724d287783
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-02-08 07:55:06 -0300

    Implementing and using randomHail function to return a random hail message
    Moreover,
    slice ([]string {va1, val2, ..., valn,}) looks like an ArrayList in Java

--- create-module/greetings.go
+++ create-module/greetings.go
@@ -15,8 +15,21 @@ func Hail(name string) (string, error) {
 	}
 
 	// var message string (":=" below does the same. It declares and initialize a variable in one line. The value in right determines the "type")
+	message := fmt.Sprintf(randomHail(), name)
 	return message, nil
 }
+
+func randomHail() string {
+	formats := []string{
+		"Hail to the king, %v!",
+		"What we do in life echoes in eternity, %v!",
+		"Imagine where you will be, %v, and it will be so!",
+		"If you find yourself alone, %v, riding in the green fields with the sun on your face, do not be troubled. For you are in Elysium, and you're already dead!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }

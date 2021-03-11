53fe3bc216ea4e1fcc472c23a0b76da296778dbe
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-03-11 07:28:12 -0300

    Creating a function that marshal (serialize) a protobuf message to binary

---
+++ grpc-pcbook/serializer/file.go
@@ -0,0 +1,22 @@
+package serializer
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/protobuf/proto"
+)
+
+//Writes Protocol Buffer message to binary file
+func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("Can't marshal proto message to binary: %w", err) //%w ~ %v (value in default format)
+	}
+	err = ioutil.WriteFile(filename, data, 0644) //ioutil.WriteFile(filename string, data []byte, perm os.FileMode)
+	if err != nil {
+		return fmt.Errorf("Can't write binary data to file %w", err)
+	}
+	return nil
+}

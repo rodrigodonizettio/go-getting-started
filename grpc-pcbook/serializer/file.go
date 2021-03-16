46f3fdfc82510c656687e4ff87a784d96c24ccde
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-03-16 07:52:14 -0300

    Changing error message in WriteProtobufToBinaryFile function

--- grpc-pcbook/serializer/file.go
+++ grpc-pcbook/serializer/file.go
@@ -15,7 +15,7 @@ func WriteProtobufToBinaryFile(message proto.Message, filename string) error {
 	}
 	err = ioutil.WriteFile(filename, data, 0644) //ioutil.WriteFile(filename string, data []byte, perm os.FileMode)
 	if err != nil {
-		return fmt.Errorf("Can't write binary data to file %w", err)
+		return fmt.Errorf("Can't write binary data to file: %w", err)
 	}
 	return nil
 }

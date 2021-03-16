efa2e1797e5b8adeadc4d373e9ed494730e4417a
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-03-16 07:56:13 -0300

    Adding proto.Message to JSON conversion to be tested

--- grpc-pcbook/serializer/file_test.go
+++ grpc-pcbook/serializer/file_test.go
@@ -13,6 +13,7 @@ func TestFileSerializer(t *testing.T) {
 	t.Parallel() //Tests can run in parallel
 
 	binaryFile := "../tmp/laptop.bin"
+	jsonFile := "../tmp/laptopJson.json"
 
 	laptop1 := sample.NewLaptop()
 	err := WriteProtobufToBinaryFile(laptop1, binaryFile)
@@ -22,4 +23,7 @@ func TestFileSerializer(t *testing.T) {
 	err = ReadProtobufFromBinaryFile(binaryFile, laptop2)
 	require.NoError(t, err)
 	require.True(t, proto.Equal(laptop1, laptop2))
+
+	err = WriteProtobufToJsonFile(laptop1, jsonFile)
+	require.NoError(t, err)
 }
